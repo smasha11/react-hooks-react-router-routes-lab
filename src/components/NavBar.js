dea05f66e558332a1d94e1dca2cb82a722225c6e
smasha11 <ismail.hussein.1120@gmail.com>
2024-07-22 10:42:46 +0300

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,61 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  margin: "0 10px",
+  textDecoration: "none",
+  color: "blue",
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          fontWeight: "bold",
+          color: "red",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          fontWeight: "bold",
+          color: "red",
+        }}
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          fontWeight: "bold",
+          color: "red",
+        }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          fontWeight: "bold",
+          color: "red",
+        }}
+      >
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
